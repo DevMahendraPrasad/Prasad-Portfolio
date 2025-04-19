de2fb65e0fb536f6329cf2d43fa5f3dcb94baab9
Mahendra prasad <118207059+DevMahendraPrasad@users.noreply.github.com>
2025-04-19 14:33:59 +0530

    Update sript.js

--- sript.js
+++ sript.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["FULL STACK Developer", "JAVA"],
+    strings: ["Full Stack Developer", "Java Developer"],
     typeSpeed: 100,
     backSpeed:100,
     backDelay: 1000,
